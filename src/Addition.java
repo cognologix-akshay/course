8c195601079459f3cdbb551b7e6e990c81a38c01
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-08-18 16:55:08 +0530

    commiting

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+import java.lang.*;
+import java.util.*;
+
+public class Addition {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+
+        c=a+b;
+
+        System.out.println("The addition of three number is =" + c);
+    }
+}
