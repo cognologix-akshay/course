52d24a2d97f809aa1fcbd4d8b2ceef5dacd24ba7
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-08-20 15:18:36 +0530

    if then statement

---
+++ src/main/java/ifthenstatement.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+public class ifthenstatement {
+
+        public static void main(String[] args) {
+
+            boolean result = false;
+            if (result == true)
+                System.out.println("it is false");
+                System.out.println("it is true");
+        }
+    }
+
+
+
