4cdea14ceee4b01fce7e69ded8443246efec0ddf
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-08-25 14:34:16 +0530

    AreEqualByThreeDecimalPlace

---
+++ src/main/java/com/test/AreEqualByThreeDecimalPlace.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class AreEqualByThreeDecimalPlace {
+
+    public static boolean Compare(double no1, double no2) {
+
+        float a = (float) no1 * 1000;
+        float b = (float) no2 * 1000;
+
+        int c = (int) a;
+        int d = (int) b;
+
+        if (c == d) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean ret = Compare(3.125, 3.1257);
+        System.out.println(ret);
+
+    }
+}
