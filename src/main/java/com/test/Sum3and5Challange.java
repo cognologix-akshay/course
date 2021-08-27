ab9e07bb395d071773b96703626ae6524c095c1b
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-08-27 18:12:07 +0530

    Sum3and5Challange

--- src/main/java/com/test/Sum3and5Challange.java
+++ src/main/java/com/test/Sum3and5Challange.java
@@ -1,17 +1,21 @@
-// The number which is divisible by 3 as well as 5 ,range is in between 1 to 1000
+// The number which is divisible by 3 as well as 5  and calulate sum ,range is in between 1 to 100
 
 package com.test;
 
 public class Sum3and5Challange {
     public static void main(String []args)
     {
-        for(int i=1;i<1000;i++)
+        int sum=0;
+        for(int i=1;i<100;i++)
         {
             if((i%3==0)&&(i%5==0))
             {
+                sum=sum+i;
                 System.out.println("The number is divisible by 3 and 5 is ="+i );
             }
         }
+        System.out.println("sum is = "+ sum);
+
 
     }
 }
