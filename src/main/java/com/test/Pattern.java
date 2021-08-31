bf3463092567dec97fe7b590a541b90888cc036a
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-08-31 18:27:13 +0530

    Pattern

---
+++ src/main/java/com/test/Pattern.java
@@ -0,0 +1,22 @@
+package com.test;
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args)
+    {
+        int i=0;
+        int j=0;
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int no=sobj.nextInt();
+
+        for(i=0;i<no;i++)
+        {
+            for(j=0;j<no;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
