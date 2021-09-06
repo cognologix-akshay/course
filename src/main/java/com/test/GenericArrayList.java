ea92c66d48419f1847a6c2a66669a2719175709e
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-09-06 19:53:48 +0530

    GenericArrayList

---
+++ src/main/java/com/test/GenericArrayList.java
@@ -0,0 +1,24 @@
+package com.test;
+import java.lang.*;
+import java.util.*;
+import java.util.ArrayList;
+
+class  GenArr<X,Y>
+{
+    X name;
+    Y no;
+    void Add(X name,Y no)
+    {
+        System.out.println(name +""+ no);
+    }
+}
+
+public class GenericArrayList {
+    public static void main(String[] args) {
+        java.util.ArrayList<String> aobj=new ArrayList<String>();
+        GenArr<String,Integer> hobj=new GenArr<>();
+        hobj.Add("akshay",1234);
+
+    }
+
+}
