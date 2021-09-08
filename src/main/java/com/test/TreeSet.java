104f0c004cb61825ba8662adf3ade138d7432860
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-09-08 20:35:34 +0530

    TreeSet

---
+++ src/main/java/com/test/TreeSet.java
@@ -0,0 +1,16 @@
+package com.test;
+import java.util.Set;
+
+public class TreeSet {
+    public static void main(String []Args)
+    {
+        Set<Integer> sobj=new java.util.TreeSet<>();
+
+        sobj.add(400);
+        sobj.add(4);
+        sobj.add(288);
+        sobj.add(450);
+
+        System.out.println(sobj);
+    }
+}
