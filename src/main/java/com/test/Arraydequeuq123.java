1900399ce6cd841a51c5b42325427de7f10c8a69
Akshay_Ambure <akshay.ambure@cognologix.com>
2021-09-17 19:12:31 +0530

    Arraydequeuq123

---
+++ src/main/java/com/test/Arraydequeuq123.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Arraydequeuq123 {
+    public static void main(String[] Args) {
+
+        Queue<Integer> obj1=new ArrayDeque<>();
+
+        obj1.add(3);
+        obj1.add(33);
+        obj1.add(8);
+        obj1.add(4);
+        obj1.remove();
+        System.out.println(obj1);
+
+
+    }
+}
